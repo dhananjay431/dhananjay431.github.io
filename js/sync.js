40bc0e70965ecfcae63bdc7372869448177c3a8e
dbel431 <dbel431@gmail.com>
2019-10-03 17:07:54 +0530

    change

--- js/sync.js
+++ js/sync.js
@@ -1,1 +1,1 @@
-"use strict";!function(n){n.db=function(){},n.db.sync=function(n,i,c){n.length<1?i(c):(t=n.shift(),new Promise(function(n,i){t(c,n,i)}).then(function(t){db.sync(n,i,t)},function(n){i(c)}))}}(window);
+"use strict";!function(n){n.db=function(){},n.db.sync=function(n,i,t){if(n.length<1)i(t);else{var c=n.shift();new Promise(function(n,i){c(t,n,i)}).then(function(t){db.sync(n,i,t)},function(n){i(t)})}}}(window);
